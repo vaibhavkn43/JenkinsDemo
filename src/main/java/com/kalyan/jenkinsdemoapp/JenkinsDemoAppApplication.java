64f5db095745cb98de81fd1dd9d31c94f7121e3c
vaibhav <vaibhavkn43@gmail.com>
2022-09-04 15:58:08 +0530

    first commit

---
+++ src/main/java/com/kalyan/jenkinsdemoapp/JenkinsDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.kalyan.jenkinsdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalyan/jenkinsdemoapp/JenkinsDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalyan.jenkinsdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
